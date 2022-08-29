7b1aedc29e3fea2ee13b0c002edf1382a695088d
JingShing <JingShingLai@gmail.com>
2022-08-29 21:26:57 +0800

    Add files via upload

---
+++ first_connect_game/server.py
@@ -0,0 +1,40 @@
+import socket
+from _thread import *
+import sys
+
+server = ""
+# use ipconfig in cmd to find your own ip in ipv4
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+# 2 client to connet to my server
+print("Waiting for a connection, Server Started")
+
+def threaded_client(connection):
+    reply = ""
+    while True:
+        try:
+            data = connection.recv(2048)
+            reply = data.decode("utf-8")
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+            connection.sendall(str.encode(reply))
+        except:
+            break
+
+while True:
+    connection, address = s.accept()
+    print("Connected to: ", address)
+
+    start_new_thread(threaded_client, (connection, ))
